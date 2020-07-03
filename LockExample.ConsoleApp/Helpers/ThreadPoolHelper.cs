bed4f9dcf07a41772636846833e3a866b020fd96
Arnab Roy Chowdhury <arnab-roy-chowdhury@outlook.com>
2020-07-03 12:45:32 +0530

    Convert variable to property.

--- LockExample.ConsoleApp/Helpers/ThreadPoolHelper.cs
+++ LockExample.ConsoleApp/Helpers/ThreadPoolHelper.cs
@@ -5,18 +5,18 @@ namespace LockExample.ConsoleApp.Helpers
 {
     internal class ThreadPoolHelper
     {
-        private readonly ManualResetEvent _doneEvent;
+        private ManualResetEvent DoneEvent { get; }
 
         public ThreadPoolHelper(ManualResetEvent doneEvent)
         {
-            _doneEvent = doneEvent;
+            DoneEvent = doneEvent;
         }
 
         public void ThreadPoolCallback(object? state)
         {
             if (state == null) return;
             SomeUtility.CurrentInstance.PerformSomeOperation((int)state);
-            _doneEvent.Set();
+            DoneEvent.Set();
         }
     }
 }
